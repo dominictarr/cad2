4811bd9f673ac20d7ecf3d72f06c219cef743535
Dominic Tarr <dominic.tarr@gmail.com>
2017-12-30 21:04:00 +1300

    color sides by what panel they'd be in

--- smooth.js
+++ smooth.js
@@ -28,7 +28,7 @@ var smooth = require('./midpoint').smooth
 var chines = stations[0].map(function (_chine, i) {
   return smooth(stations.map(function (station) {
     return station[i]
-  }), 1, 0.75)
+  }), 1, 0.5)
 })
 
 console.log('chines', JSON.stringify(chines))
@@ -39,6 +39,67 @@ var stations2 = chines[0].map(function (e, i) {
   })
 })
 
+var CSG = require('@jscad/csg').CSG
+
+function rainbowByNormal (shape) {
+  shape.polygons.forEach(function (p) {
+    var n = p.plane.normal
+    p.shared = new CSG.Polygon.Shared([(n.x+1)/2,(n.y+1)/2,(n.z+1)/2, 0.5])
+    //[Math.random(),Math.random(),Math.random(), 0.5])
+  })
+  return shape
+}
+
+function eachPair (set, iter) {
+  for(var i = 0; i < set.length; i += 2)
+    iter(set[(i-1+set.length)%set.length], set[i], i)
+}
+
+function polygonByVertice (shape) {
+  var index = {}
+  shape.polygons.forEach(function (poly) {
+    poly.vertices.forEach(function (vertex) {
+      var id = vertex.getTag() //[a.toString(), b.toString()].sort().join(':')
+      if(!index[id]) index[id] = [poly]
+      else index[id].push(poly)
+    })
+  })
+  return index
+}
+
+function rainbowByPanel (shape, bend) {
+  console.log(polygonByVertice(shape))
+  bend = bend || 0.9
+  var index = polygonByVertice(shape)
+
+  function addToSide(p, shared) {
+    if(p.shared.color) return
+    p.shared = shared
+    p.vertices.forEach(function (v) {
+      index[v.getTag()].forEach(function (poly) {
+        if(poly.plane.normal.dot(p.plane.normal) < bend) return
+        addToSide(poly, shared)
+      })
+    })
+  }
+
+  //only the best colors
+  var colors = [
+    new CSG.Polygon.Shared([0,0,1, 0.5]),
+    new CSG.Polygon.Shared([0,1,0, 0.5]),
+    new CSG.Polygon.Shared([1,0,0, 0.5]),
+    new CSG.Polygon.Shared([0,1,1, 0.5]),
+    new CSG.Polygon.Shared([1,1,0, 0.5]),
+    new CSG.Polygon.Shared([1,0,1, 0.5])
+]
+
+  shape.polygons.forEach(function (poly) {
+    if(poly.shared.color) return
+    addToSide(poly, colors.shift())
+  })
+
+  return shape
+}
 
 console.log(JSON.stringify(stations2))
 //console.log(JSON.stringify(o))
@@ -49,21 +110,9 @@ console.log(JSON.stringify(stations2))
 
 module.exports = function (_) {
   function poly (out) {
-    console.log('OUT', JSON.stringify(out))
     return _.CSG.Polygon.createFromPoints(out) //, null, _.CSG.Plane.fromPoints.apply(null, out.slice().reverse()))
   }
-  return M = require('./stations')(_)(stations2.map(poly))
-
-  window.csg = _
-  var model = poly(m.snapY(o, 2)).extrude(new _.CSG.Vector3D(0,0,1)).union(
-    poly(m.snapY(o2, 2)).extrude(new _.CSG.Vector3D(0,0,1)).translate(new _.CSG.Vector3D(0,0,3))
-  )
-  window.M = model
-  return model
+//  return M = rainbowByNormal(require('./stations')(_)(stations2.map(poly)))
+  return M = rainbowByPanel(require('./stations')(_)(stations2.map(poly)))
 }
-
-
-
-
-
 
