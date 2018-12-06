415824e42e0034476ff865d4864620d309e2be32
Dominic Tarr <dominic.tarr@gmail.com>
2018-12-06 15:13:46 +1300

    6mm rope

--- pulley.js
+++ pulley.js
@@ -1,12 +1,20 @@
 
+function o (s) {
+  return s  - 0.2
+}
+function i (s) {
+  return s  + 0.2
+}
+
 module.exports = function (_) {
   function pulley () {
-    var rope_diameter = 5;
-    var pulley_diameter = 28
-    var width = 9.5 //rope_diameter * 1.2
-    var axel_diameter = 8
+    var rope_diameter = o(6);
+    var pulley_diameter = o(28)
+    var width = o(9.5) //rope_diameter * 1.2
+    var axel_diameter = i(8)
+    var clearance = 0.5
 
-    var half_width = (width)/2
+    var half_width = (width)/2 - clearance
 
     return (
       _.CAG.rectangle({corner1: [pulley_diameter/2, half_width], corner2: [0, -half_width]})
@@ -15,19 +23,27 @@ module.exports = function (_) {
         )
         .rotateExtrude({resolution: 90})
     //    .extrude([0,0,0.1])
+      .union(
+        _.CSG.cylinder({
+          radius: axel_diameter,
+          start: [0, 0, -width/2],
+          end: [0, 0, width/2]
+        })
+      )
       .subtract(
         _.CSG.cylinder({
           radius: axel_diameter/2,
-          start: [0, 0, -pulley_diameter/2],
-          end: [0, 0, pulley_diameter/2]
+          start: [0, 0, -pulley_diameter],
+          end: [0, 0, pulley_diameter]
         })
       )
     )
   }
 
 
-  return pulley()
+  return pulley().scale([1,1,1/1.022])
 }
+
 
 
 
